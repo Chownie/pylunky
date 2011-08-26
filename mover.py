5393ba3a66f0744dee100190df5404b4df6f7c74
chownplusx <sunderphon@gmail.com>
2011-08-26 19:06:31 +0200

    Added mover changes from one[coding] (Thanks!)

--- mover.py
+++ mover.py
@@ -4,6 +4,9 @@ class Mover():
 	def __init__(self, x, y, direction, speed, image):
 		self.x = 0
 		self.y = 0
+		self.w = 32
+		self.h = 32
 		self.direction = 0
 		self.speed = 0
-		self.image = pygame.image.load('./resources/a.jpg')
+		self.jump = -1
+		self.image = pygame.image.load('./resources/a.png')
