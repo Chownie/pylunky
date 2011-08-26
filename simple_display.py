379796d8761c4c72f32263a5393f1485bc27e9e6
chownplusx <sunderphon@gmail.com>
2011-08-26 19:05:17 +0200

    Added gravity+collision changes from one[coding] (Thanks!)

--- simple_display.py
+++ simple_display.py
@@ -17,41 +17,79 @@ clock = pygame.time.Clock()
 
 global collisions
 
-def controls(key, puppet, mapinfo):
+def gTile(x, y):
+	return mapinfo.tile(x,y)
+
+def oMove(o, x, y):	
+	
+	if x>0:
+		tempx = o.x + x 
+		if gTile(round((tempx+31)/32), round((o.y+1)/32)).soli == False and gTile(round((tempx+31)/32), round((o.y+31)/32)).soli == False:
+			o.x = o.x + x
+		
+	if x<0:
+		tempx = o.x + x 
+		if gTile(round((tempx+1)/32), round((o.y+1)/32)).soli == False and gTile(round((tempx+1)/32), round((o.y+31)/32)).soli == False:
+			o.x = o.x + x
+			
+	if y>0:
+		tempy = o.y + y 
+		if gTile(round((o.x+1)/32), round((tempy+31)/32)).soli == False and gTile(round((o.x+31)/32), round((tempy+31)/32)).soli == False:
+			o.y = tempy
+		else:
+			o.jump = 0
+			
+	if y<0:
+		tempy = o.y + y 
+		if gTile(round((o.x+1)/32), round((tempy+1)/32)).soli == False and gTile(round((o.x+31)/32), round((tempy+1)/32)).soli == False:
+			o.y = tempy
+		else:
+			o.jump = -1		
+
+				
+def gravity(o, gravity, maxjump):	
+	if o.jump > 0 and o.jump < maxjump:
+		oMove(o, 0, -gravity)
+		o.jump += 1
+		if o.speed > 0: o.speed -= 0.1
+
+	else:
+		o.jump = -1
+		oMove(o, 0, gravity)
+		if o.speed < 1: o.speed += 0.1
+
+
+def controls(key, o):
 	pygame.event.pump()
 	
+	move = 0
+	
 	if key[pygame.K_LEFT]: 
-		tempx = puppet.x-1
-		tile = mapinfo.tile(round(tempx+16/32)/32,round(puppet.y+16/32)/32)
-		puppet.x-=collide(tile)
+		oMove(o, -1, 0)
+		move = 1
 
 	elif key[pygame.K_RIGHT]: 
-		tempx = puppet.x+32
-		tile = mapinfo.tile(round(tempx+16/32)/32,round(puppet.y+16/32)/32)
-		puppet.x+=collide(tile)
-
+		oMove(o, 1, 0)
+		move = 1
+		
 	if key[pygame.K_UP]:
-		tempy = puppet.y-1
-		tile = mapinfo.tile(round(puppet.x+16/32)/32,round(tempy+16/32)/32)		
-		puppet.y-=collide(tile)
+		oMove(o, 0, -1)
+		move = 1
 
 	elif key[pygame.K_DOWN]: 
-		tempy = puppet.y+32
-		tile = mapinfo.tile(round(puppet.x+16/32)/32,round(tempy+16/32)/32)
-		puppet.y+=collide(tile)
-		print puppet.y
+		oMove(o, 0, 1)
+		move = 1
+	
+	if key[pygame.K_SPACE]: 
+		if o.jump == 0: 
+			o.jump = 1
 		
 	if key[pygame.K_ESCAPE]: exit()
-
-def collide(tile):
-	if tile.soli == False:
-		print "FREE: "+str((tile.posx, tile.posy))
-		return 1
-	else:
-		print "COLLIDE: "+str((tile.posx, tile.posy))
-		return 0
-
+	
+			
 def main():
+	global mapinfo
+	
 	image = pygame.image.load('resources%sa.png' % os.sep)
 	newmover = mover.Mover(x=12,y=12, direction=0, speed=0, image=image.convert_alpha())
 	mapinfo = readmap.mapObj('1.map')
@@ -59,13 +97,16 @@ def main():
 	while True:
 		pygame.event.pump()
 		key = pygame.key.get_pressed()
-		if key[pygame.K_SPACE]: pygame.image.save(screen, 'screenshot.png')
-		controls(key, newmover, mapinfo)
+		#if key[pygame.K_SPACE]: pygame.image.save(screen, 'screenshot.png')
+		controls(key, newmover)
+		gravity(newmover, 4, 32)
 
+		
 		for k in mapinfo.map():
 			for i in k:						
 				screen.blit(i.mat, (i.posx*32,i.posy*32))
 		screen.blit(newmover.image, (newmover.x,newmover.y))
+		
 		pygame.display.flip()
 		clock.tick(90)
 
