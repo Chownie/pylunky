977f4af4ff63d5a186cbd72372da469be7811c2b
Asa <sunderphon@gmail.com>
2011-10-11 04:33:33 +0000

    More dir cleanup

--- resources/blocks.py
+++
@@ -1,4 +0,0 @@
-blocks = {"0":{"soli":False,"trans":True,"mat":"0.jpg"}, \
-          "1":{"soli":True,"trans":False,"mat":"1.jpg"}, \
-          "&":{"soli":False,"trans":False,"mat":"&.jpg"}, \
-          "N":{"soli":False,"trans":False,"mat":"N.jpg"}}
